8fb6e68c8a31d0e4428c4575347439d9d155aaf3
Terry Su <limacechan2169@gmail.com>
2022-10-11 19:54:35 -0400

    decode array of bytes impl

---
+++ decode.py
@@ -0,0 +1,33 @@
+TEST = ["7E | 00 19 | 10 | 01 | 00 13 A2 00 41 B1 6D 1C | FF FE | 00 | 00 | 53 45 4E 54 20 46 52 4F 4D 20 42 | D1"]
+
+def decode(msg, BASE=16):
+
+    #preprocess and partition raw string
+    msg = msg.split('|')
+    msg = [x.strip() for x in msg]
+    msg = [x.split(' ') for x in msg]
+
+    if msg[0][0] != '7E': return False #no start delimeter
+
+    #length of message
+    length = ''.join(msg[1])
+    length = int(length, BASE)
+
+    #other relevant info
+    frame_type = msg[2][0]
+    frame_id = msg[3][0]
+    address_64_bit = msg[4][0]
+    address_16_bit = msg[5][0]
+    brodcast_radius = msg[6][0]
+    options = msg[7][0]
+
+    #decode bytes and return message content
+    msg_content = bytes.fromhex(' '.join(msg[8]))
+    msg_content = bytes.decode(msg_content)
+
+    return msg_content
+
+__name__ = '__decode__'
+if __name__ == '__decode__':
+    for msg in TEST: print(decode(msg))
+
