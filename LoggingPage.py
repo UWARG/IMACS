7480f68c7a4630407a2ae023c5583c63be58fc2e
HarryFung22 <106692548+HarryFung22@users.noreply.github.com>
2022-10-24 10:47:41 -0400

    textbox for logging page

--- LoggingPage.py
+++ LoggingPage.py
@@ -1,15 +1,41 @@
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+import sys
+
 
 class LoggingPage(QWidget):
     def __init__(self, stack):
         super(LoggingPage, self).__init__()
 
         self.stack = stack
+        layout = QVBoxLayout()
 
         # Create the Logging page UI here
-        layout = QHBoxLayout()
-        layout.addWidget(QLabel("Logging Page"))
         
+        self.textbox = QPlainTextEdit()
+        layout.addWidget(self.textbox)
+        
+        layout.addWidget(QLabel("Logging Page"))
         self.stack.setLayout(layout)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyleSheet('''
+        QWidget {
+            font-size: 30px;
+        }
+
+    ''')
+
+    loggingpage = LoggingPage()
+    loggingpage.show()
+
+
+
+
+    
+    
+        
+
+        
