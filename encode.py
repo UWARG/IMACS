1656b099975a559060d67680a8080fb3d9956835
Terry Su <limacechan2169@gmail.com>
2022-10-30 18:15:47 -0400

    fix encode func

--- encode.py
+++ encode.py
@@ -8,12 +8,15 @@ def encode(msg):
     encoded_msg = str(encoded_msg, 'ascii')
     encoded_msg = encoded_msg.upper()
 
-    res = []
+    msg_str = []
     for x in encoded_msg: 
-        if not res or len(res[-1])==2: res += [x]
-        else: res[-1] += x
+        if not msg_str or len(msg_str[-1])==2: msg_str += [x]
+        else: msg_str[-1] += x
+    msg_str = ' '.join(msg_str)
 
-    return ' '.join(res)
+    res = ['7E','00 19','10','01','00 13 A2 00 41 B1 6D 1C','FF FE', '00', '00', msg_str, 'D1'] #note: other properties currently harcoded
+
+    return ' | '.join(res)
 
 __name__ = '__encode__'
 if __name__ == '__encode__':
