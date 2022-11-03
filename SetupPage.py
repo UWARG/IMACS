52655425d1779f207fd382fe31770d971b5e31b4
YMRTZ <a76yang@uwaterloo.ca>
2022-11-02 21:36:50 -0400

    SetupPage work

--- SetupPage.py
+++ SetupPage.py
@@ -3,6 +3,9 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 class SetupPage(QWidget):
+
+    
+
     def __init__(self, stack):
         super(SetupPage, self).__init__()
 
@@ -10,6 +13,44 @@ class SetupPage(QWidget):
 
         # Create the Setup page UI here
         layout = QHBoxLayout()
-        layout.addWidget(QLabel("Setup Page"))
-        
+
+        formLayout = QHBoxLayout()
+        makeForm(formLayout)
+
+        mapLayout = QHBoxLayout()
+        mapLayout.addWidget(QLabel("Map"))
+
+        layout.addLayout(formLayout)
+        layout.addLayout(mapLayout)
+
+        self.createWaypointGrid(formLayout)
+
         self.stack.setLayout(layout)
+
+        # TO-DO: QFormLayout shell
+
+    def createWaypointGrid(self,formLayout):
+        gridLayout = QGridLayout()
+        gridShell = QFormLayout()
+        self.showWaypointButton = QPushButton("+")
+        self.hideWaypointButton = QPushButton("-")
+        gridLayout.addWidget(self.showWaypointButton,0,0)
+        gridLayout.addWidget(self.hideWaypointButton,0,1)
+        gridShell.addRow(gridLayout)
+        formLayout.addLayout(gridShell)
+
+def makeForm(formLayout):
+    theFormLayout = QFormLayout()
+
+    rcComboBox = QComboBox()
+
+    rcComboBox.addItems(["1", "2", "3"])
+
+    lineEdit = QLineEdit()
+
+    theFormLayout.addRow(QLabel("Setup"))
+    theFormLayout.addRow(QLabel("RC Link"), rcComboBox)
+    theFormLayout.addRow(QLabel("Bidirectional data \n telemetry"), lineEdit)
+    theFormLayout.addRow(QLabel("Waypoint"))
+
+    formLayout.addLayout(theFormLayout) 
