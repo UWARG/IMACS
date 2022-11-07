11cec8578ab0e0ecd7d5dd38105f12e1d9fc0154
m6keller <kellermatthewe@gmail.com>
2022-11-06 22:33:05 -0500

    encodes to bytes rather than string. example payload message included

--- encode.py
+++ encode.py
@@ -1,23 +1,52 @@
-import binascii
+import struct
 
-TEST = ['SENT FROM B']
+AIR_GROUND_PAYLOAD_TEST_MESSAGE = {
+    'time': 1,
+    'payload_type': 1,
+    'crc': 1,
+    'gps_data': {
+        'lat': 1.0,
+        'lon': 1.0,
+        'alt': 1.0,
+    },
+    'imu_data': {
+        'yaw': 1.0,
+        'pitch': 1.0,
+        'roll': 1.0,
+    },
+    'motor_outputs': [1,2,3,4,5,6,7,8,9,10,11,12]
+}
 
 def encode(msg):
 
-    encoded_msg = binascii.hexlify(b'SENT FROM B')
-    encoded_msg = str(encoded_msg, 'ascii')
-    encoded_msg = encoded_msg.upper()
+    encoded_msg = payload_to_bytes(msg)
 
-    msg_str = []
-    for x in encoded_msg: 
-        if not msg_str or len(msg_str[-1])==2: msg_str += [x]
-        else: msg_str[-1] += x
-    msg_str = ' '.join(msg_str)
+    res = b'\x7e\x00\x19\x10\x01\x00\x13\xa2\x00\x41\xb1\x6d\x1c\xff\xfe\x00\x00' + encoded_msg + b'\xd1'
+    return res
 
-    res = ['7E','00 19','10','01','00 13 A2 00 41 B1 6D 1C','FF FE', '00', '00', msg_str, 'D1'] #note: other properties currently harcoded
+def payload_to_bytes(payload):
+    encoded_payload = b''
+    for value in payload.values():
+        if type(value) == dict:
+            encoded_payload += payload_to_bytes(value)
+        elif type(value) == list:
+            for v in value:
+                encoded_payload += value_to_bytes(v)
+        else:
+            encoded_payload += value_to_bytes(value)
+       
+    return encoded_payload
 
-    return ' | '.join(res)
+def value_to_bytes(value):
+    if type(value) == float:
+        return struct.pack('<f', value)
+    elif type(value) == int:
+        return bytes([value])
+    else:
+        raise Exception('Unknown type')
 
-__name__ = '__encode__'
-if __name__ == '__encode__':
-    for msg in TEST: print(encode(msg))
+    
+if __name__ == '__main__':
+    print(encode(AIR_GROUND_PAYLOAD_TEST_MESSAGE))
+
+
