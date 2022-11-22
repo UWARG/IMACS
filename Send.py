b5bd770c3c2f071583caac10251ae45b50b832a0
m6keller <kellermatthewe@gmail.com>
2022-11-22 16:25:24 -0500

    send method in Send class wraps message with proper frame

--- Send.py
+++ Send.py
@@ -31,8 +31,13 @@ class Send():
             raise Exception('Unknown type')
     
     @staticmethod
-    def send(payload):
+    def send(payload, type):
         encoded_payload = Send.__encode(payload)
-        return encoded_payload
+        # should this include the length of bytes in info or entire frame?
+        length = len(encoded_payload) # + 8 # 8 bytes for length and type and start and end 
+        wrapped_payload = b'\x7e' + struct.pack("H", length) + struct.pack("B", type) + b'\x00\x19\x10\x01\x00\x13\xa2\x00\x41\xb1\x6d\x1c\xff\xfe\x00\x00' + encoded_payload + b'\xd1' # last should be crc
+        
+        print("wrapped_payload")
+        return wrapped_payload
         # should actually send payload thru rfds
         
