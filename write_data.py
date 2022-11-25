04347814f410e2f60d5b1dc3394dd8a7ee008fae
m6keller <kellermatthewe@gmail.com>
2022-11-24 21:04:59 -0500

    log data from serial port

--- write_data.py
+++ write_data.py
@@ -1,17 +1,30 @@
-import serial
+# ls -l /dev/cu.usb*
 
-BAUDRATE = 9600
+import serial
+import calendar
+import time
+
+BAUDRATE = 115200
 BYTE_SIZE = 8
 TIMEOUT = 2
 
 FILENAME = "text_log.txt"
+PORT = "/dev/cu.usbmodem14101"
 
 file = open(FILENAME, "a")
 
-rfd = serial.Serial(port="/dev/cu.usbmodem14101", baudrate=BAUDRATE, bytesize=BYTE_SIZE, timeout=TIMEOUT, stopbits=serial.STOPBITS_ONE)
+rfd = serial.Serial(port=PORT, baudrate=BAUDRATE, bytesize=BYTE_SIZE, timeout=TIMEOUT, stopbits=serial.STOPBITS_ONE)
+
+
 
 while True:
-    if rfd.read(1) == b'\x7e':
-        file.write(rfd.read(6))
+    timestamp = calendar.timegm(time.gmtime())
+    first_hex = rfd.read(2).hex()
+    second_hex = rfd.read(2).hex()
+
+    file.write(str(timestamp) + ": " + str(first_hex) + " " + str(second_hex) + "\n")
+    print(str(first_hex) + " " + str(second_hex))
 
 file.close()
+
+# later, code should do file comparisons
