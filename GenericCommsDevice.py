962aab7507b4cd2972645d23d05571739f45b07f
Aaditya Chaudhary <a84chaud@uwaterloo.ca>
2023-01-23 23:46:39 -0500

    got code generation proper i think, started fleshing out generic comms driver class

--- GenericCommsDevice.py
+++ GenericCommsDevice.py
@@ -1,6 +1,22 @@
+import libscrc
+
+
 class GenericCommsDevice():
 
-    ser = None
-
     def __init__(self, port, baudrate):
-        ser = serial.Serial(port=port, baudrate=baudrate, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+        self.ser = serial.Serial(port=port, baudrate=baudrate, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+
+    def checkCRC(self, b):
+        return libscrc.crc32(b)
+
+    def transmit(b):
+        ser.write(b)
+    def recieve():
+        
+        readByte = rfd.read()
+        if readByte == 0x7e:
+            # start of new message
+            
+        self.currentMessage.append(readByte)
+        
+        
