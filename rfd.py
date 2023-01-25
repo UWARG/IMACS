0f3d1fbe60123d72cc79ba263c749eec341713e4
Aaditya Chaudhary <a84chaud@uwaterloo.ca>
2023-01-25 01:21:03 -0500

    driver working, need to clean up PR a bit

--- rfd.py
+++ rfd.py
@@ -1,13 +1,13 @@
 import serial
 
 
-rfd = serial.Serial(port="COM7", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
-rfd2 = serial.Serial(port="COM6", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+rfd = serial.Serial(port="/dev/ttyUSB0", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+# rfd2 = serial.Serial(port="COM6", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
 while(True):
-    data = [0x7e]
-    print("byte sent",data[0].hex())
-    rfd2.write(bytearray(data))
+    # data = [0x7e]
+    # print("byte sent",data[0].hex())
+    # rfd2.write(bytearray(data))
 
     readByte = rfd.read()
-    print("byte read",readByte.hex())
+    print("byte read",hex(readByte))
