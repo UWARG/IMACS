ea9c787ea0041cd4d42d980379cea66ee510ae28
Ethan Woo <108487936+ethancpwoo@users.noreply.github.com>
2023-01-29 17:05:11 -0500

    delete pd19 lol

--- HeadingWidget.py
+++ HeadingWidget.py
@@ -10,18 +10,22 @@ class HeadingWidget(QVBoxLayout):
         videoWidget = QWidget()
         videoWidget.setLayout(self.video)
         self.circlepixmap = QPixmap('src/attitude controller.png')
+
         self.view = QGraphicsView()
         self.scene = QGraphicsScene()
         self.translate = QTransform()
-        self.rotate = QTransform()
+
         self.circlegraphic_pixmap = QGraphicsPixmapItem()
         self.circlegraphic_pixmap.setPixmap(self.circlepixmap)
         self.circlegraphic_pixmap.setTransformOriginPoint(self.circlepixmap.width()/2, self.circlepixmap.height()/2)
+        
         self.scene.setBackgroundBrush(QBrush(Qt.GlobalColor.black))
         self.scene.addWidget(videoWidget)
         self.scene.addItem(self.circlegraphic_pixmap)
+        
         self.heighty = self.geometry().height()  
         self.widthx = self.geometry().width()
+        
         self.view.setSceneRect(0, 0, self.widthx, self.heighty)
         self.view.setScene(self.scene)
         self.view.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
@@ -29,11 +33,11 @@ class HeadingWidget(QVBoxLayout):
         self.addWidget(self.view)
 
     def scaled(self):
-        self.heighty = self.geometry().height()  #try maybe it is view not self...
-        self.widthx = self.geometry().width()
+        self.heighty = self.view.height()  #try maybe it is view not self...
+        self.widthx = self.view.width()
         self.scene.removeItem(self.circlegraphic_pixmap)
         self.scene.addItem(self.circlegraphic_pixmap)
-        self.translate.translate(self.scene.width()/2 - 175, self.scene.height()/10)
+        self.translate.translate(self.scene.width()/2, self.scene.height()/10)
         self.circlegraphic_pixmap.setTransform(self.translate)
         self.circlegraphic_pixmap.setRotation(15)
         #print(self.circlegraphic_pixmap.pos().x(), ' ', self.circlegraphic_pixmap.pos().y())
