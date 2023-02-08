042ea7a9f578cfc6d0bf79c764e321004cc1ea7d
m6keller <kellermatthewe@gmail.com>
2023-02-07 18:31:51 -0800

    added comments for data types

--- mock_ground_receive.py
+++ mock_ground_receive.py
@@ -23,37 +23,47 @@ class GroundReceive():
         motor_outputs_retriever = AccessData(msg=driver_packet.motor_outputs, start_index=0)
         battery_voltage_retirever = AccessData(msg=driver_packet.battery_voltages, start_index=0)
         controller_values_retriever = AccessData(msg=driver_packet.controller_values, start_index=0)
+
         self.payload = {
-            'motor_outputs': [motor_outputs_retriever.get_data(data_type="B") for i in range(0, 12)], # list of 12 uint8_t
+            'motor_outputs': [motor_outputs_retriever.get_data(data_type="B") for i in range(0, 12)], # list of 12 ints
             'gps_data': {
-                'lat' : driver_packet.data.latitude,
-                'lon' : driver_packet.data.longitude,
-                'alt' : driver_packet.data.altitude,
+                'lat' : driver_packet.data.latitude, # double
+                'lon' : driver_packet.data.longitude, # double
+                'alt' : driver_packet.data.altitude, # float
             },  
-            'climb_rate': driver_packet.data.climb_rate,
-            'heading': driver_packet.data.heading,
-            'air_speed': driver_packet.data.air_speed,
-            'ground_Speed': driver_packet.data.ground_speed,
+            'climb_rate': driver_packet.data.climb_rate, # float
+            'heading': driver_packet.data.heading, # float
+            'air_speed': driver_packet.data.air_speed, # float
+            'ground_Speed': driver_packet.data.ground_speed, # float
             'imu_data':{
-                'roll': driver_packet.data.roll,
-                'pitch': driver_packet.data.pitch,
-                'yaw': driver_packet.data.yaw,
+                'roll': driver_packet.data.roll, # float
+                'pitch': driver_packet.data.pitch, # float
+                'yaw': driver_packet.data.yaw, # float
             },
-            'roll_rate': driver_packet.data.roll_rate,
-            'pitch_rate': driver_packet.data.pitch_rate,
-            'yaw_rate': driver_packet.data.yaw_rate,
-            'battery_voltages': [battery_voltage_retirever.get_data(data_type="B") for i in range(0, 13)],
-            'controller_voltages':  [controller_values_retriever.get_data(data_type="B") for i in range(0, 16)],
+            'roll_rate': driver_packet.data.roll_rate, # float
+            'pitch_rate': driver_packet.data.pitch_rate, # float
+            'yaw_rate': driver_packet.data.yaw_rate, # float
+            'battery_voltages': [battery_voltage_retirever.get_data(data_type="B") for i in range(0, 13)], # list of 13 ints
+            'controller_voltages':  [controller_values_retriever.get_data(data_type="B") for i in range(0, 16)], # list of 16 ints
+            'flag': AccessData(msg=driver_packet.header.flag, start_index=0).get_data(data_type="B"), # int
+            'length': AccessData(msg=driver_packet.header.length, start_index=0).get_data(data_type="H"), # short
+            'type': AccessData(msg=driver_packet.header.type, start_index=0).get_data(data_type="B"), # int
         }
 
     def __decode_pid_set_response(self, driver_packet):
         self.pid_set_response = {
-            "flag": AccessData(msg=driver_packet.header.flag, start_index=0).get_data(data_type="B"),
-            "length": AccessData(msg=driver_packet.header.length, start_index=0).get_data(data_type="H"),
-            "type": AccessData(msg=driver_packet.header.type, start_index=0).get_data(data_type="B"),
-            "controller_number": AccessData(msg=driver_packet.controller_number, start_index=0).get_data(data_type="B"),
-            "controller": [ {"P": driver_packet.controller.axes[i].P, "I": driver_packet.controller.axes[i].I, "D": driver_packet.controller.axes[i].D } for i in range(0,6)],
-            "crc": AccessData(msg=driver_packet.crc, start_index=0).get_data(data_type="f"),
+            "flag": AccessData(msg=driver_packet.header.flag, start_index=0).get_data(data_type="B"), # int
+            "length": AccessData(msg=driver_packet.header.length, start_index=0).get_data(data_type="H"), # short
+            "type": AccessData(msg=driver_packet.header.type, start_index=0).get_data(data_type="B"), # int
+            "controller_number": AccessData(msg=driver_packet.controller_number, start_index=0).get_data(data_type="B"), # int
+            "controller": [  # list of 6 dicts each with three floats (P, I, D)
+                    {
+                        "P": driver_packet.controller.axes[i].P, 
+                        "I": driver_packet.controller.axes[i].I, 
+                        "D": driver_packet.controller.axes[i].D 
+                    } for i in range(0,6)
+                ],
+            "crc": AccessData(msg=driver_packet.crc, start_index=0).get_data(data_type="f"), # float
         }
 
     def __gen_mock_packet(self):
@@ -75,7 +85,7 @@ class GroundReceive():
 
     def mock_receive(self):
         while True:
-            if random.randint(0, 10000000) == 1:
+            if random.randint(0, 1000000) == 1:
                 mock_packet = self.__gen_mock_packet()
                 self.__decode(mock_packet)
             if self.pid_set_response != None:
