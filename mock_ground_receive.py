678f3c0b6de55fe3156410e274e8f2096c383043
m6keller <kellermatthewe@gmail.com>
2023-02-09 18:22:52 -0800

    classes imported from rfd_driver

--- mock_ground_receive.py
+++ mock_ground_receive.py
@@ -1,8 +1,9 @@
 import random
 from AccessData import AccessData 
 from rfd_driver.generic_comms_device import GenericCommsDevice
+from rfd_driver.TelemMessages.GroundStationData import GroundStationData
+from rfd_driver.TelemMessages.GroundStationPIDSetResponse import GroundStationPIDSetResponse
 
-from structs import GroundStationData, SensorData, PIDValues, PIDController, GroundStationPIDSetResponse, Header
 
 class GroundReceive():
     def __init__(self, ground_station_data=None, pid_set_response=None, port="/dev/ttyUSB0", baudrate=115200):
@@ -68,35 +69,3 @@ class GroundReceive():
                 ],
             "crc": AccessData(msg=driver_packet.crc, start_index=0).get_data(data_type="f"), # float
         }
-
-    # This is just for mocking messages for gui
-    def __gen_mock_packet(self):
-        if random.randint(0,1) == 1:
-            header = Header(b'\x00', b'\x00\x01', b'\x01')
-            sensor_data = SensorData(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0)
-            motor_outputs = b'\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01'
-            battery_voltages = b'\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00'
-            controller_values = b'\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01'
-            crc = b'\x00\x01\x00\x01\x00\x01'
-            return GroundStationData(header=header, motor_outputs=motor_outputs, data=sensor_data, battery_voltages=battery_voltages, controller_values=controller_values, crc=crc)
-        else: 
-            header = Header(b'\x00', b'\x00\x01', b'\x01')
-            controller_number = b'\x00'
-            crc = b'\x00\x01\x00\x01\x00\x01'
-            pid_values = [PIDValues(1.0 + i, 2.0 + i, 3.0 + i) for i in range(0, 6)]
-            controller = PIDController(pid_values)
-            return GroundStationPIDSetResponse(header, controller_number, controller, crc)
-
-    # This is just for mocking messages for gui
-    def mock_receive(self):
-        while True:
-            if random.randint(0, 1000000) == 1:
-                mock_packet = self.__gen_mock_packet()
-                self.__decode(mock_packet)
-            if self.pid_set_response != None:
-                print(self.pid_set_response)
-            if self.payload != None:
-                print(self.payload)
-        
-
-
